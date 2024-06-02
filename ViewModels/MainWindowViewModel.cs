9a6b4a61cef5109742470033d7a655ea8ad7873d
Master-of-numbers <myros.users@gmail.com>
2024-06-02 16:23:57 +0300

    testing features added

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,8 +1,151 @@
-﻿namespace LLDock.ViewModels;
+﻿using System;
+using System.Collections.ObjectModel;
+using ReactiveUI;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows.Input;
+using Avalonia.Controls;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+using LLDock.Views;
+
+namespace LLDock.ViewModels;
 
 public class MainWindowViewModel : ViewModelBase
 {
 #pragma warning disable CA1822 // Mark members as static
-    public string Greeting => "Welcome to Avalonia!";
+    
 #pragma warning restore CA1822 // Mark members as static
+
+    #region Variables
+
+    private int _panelLength;
+    private static int _topMargin;
+    public int DisplayHeight;
+    private ObservableCollection<string> _runningApplications;
+
+    public int PanelLength
+    {
+        get => _panelLength;
+        set => this.RaiseAndSetIfChanged(ref _panelLength, value);
+    }
+    public static int TopMargin
+    {
+        get => _topMargin;
+        set => _topMargin = value;
+    }
+    public ObservableCollection<string> RunningApplications
+    {
+        get => _runningApplications;
+        set => this.RaiseAndSetIfChanged(ref _runningApplications, value);
+    }
+
+    #endregion
+
+    public MainWindowViewModel()
+    {
+        StartApplicationCommand = ReactiveCommand
+            .Create(() => StartApp("C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"));
+        OpenSettingsWindowCommand = ReactiveCommand
+            .Create(OpenSettingsWindow);
+        MainWindow.PanelShowsEvent += CheckAppsRunning;
+        
+        ButtonBackColor = "#424242";
+        PanelLength = 400;
+        TopMargin = (MainWindow.DisplayHeight - PanelLength) / 2;
+        
+        RunningApplications = new ObservableCollection<string>();
+
+    }
+    #region Button commands
+
+    public ICommand StartApplicationCommand { get; set; }
+    public ICommand OpenSettingsWindowCommand { get; set; }
+    
+
+    #endregion
+    #region Properties
+
+    private string _buttonBackColor;
+    public string ButtonBackColor
+    {
+        get => _buttonBackColor;
+        set => this.RaiseAndSetIfChanged(ref _buttonBackColor, value);
+    }
+    
+    #endregion
+    #region Methods
+    
+    private void StartApp(string path)
+    {
+        try
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = path,
+                UseShellExecute = true // Це важливо для запуску GUI додатків
+            };
+
+            Process process = new Process
+            {
+                StartInfo = startInfo
+            };
+            process.Start();
+            Views.MainWindow.RaiseStaticEvent();
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Error while starting: {ex.Message}");
+        }
+        GetRunningApps();
+    }
+
+    public void CheckAppsRunning()
+    {
+        var processes = Process.GetProcessesByName("chrome");
+        if (processes.Any())
+        {
+            ButtonBackColor = "#888888";
+        }
+        else
+        {
+            ButtonBackColor = "#424242";
+        }
+    }
+
+    private void GetRunningApps()
+    {
+        RunningApplications.Clear();
+
+        // Get all running processes
+        var processes = Process.GetProcesses();
+
+        // Add process names to the collection
+        foreach (var process in processes.OrderBy(p => p.ProcessName))
+        {
+            try
+            {
+                if (!string.IsNullOrEmpty(process.MainWindowTitle))
+                {
+                    RunningApplications.Add(process.MainWindowTitle);
+                    Debug.WriteLine(process.ToString());
+                    //Debug.WriteLine(process.ProcessName);
+                }
+            }
+            catch
+            {
+                // Handle any exceptions thrown by accessing process properties
+            }
+        }
+    }
+    public void OpenSettingsWindow()
+    {
+        var settingWindow = new SettingsWindow();
+        settingWindow.Show();
+    }
+    
+    #endregion
+    
+    
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,11 +1,122 @@
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Input;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Input;
+using Avalonia.Layout;
+using Avalonia.Platform;
+using Avalonia.Threading;
+using Avalonia.Win32.Interop;
+using LLDock.ViewModels;
+using ReactiveUI;
 
 namespace LLDock.Views;
-
 public partial class MainWindow : Window
 {
+    #region Variables
+
+    //private Point _lockedPosition;
+
+    public static int DisplayHeight;
+    
+    public DispatcherTimer PanelHidingTimer;
+    public static event Action PanelShowsEvent;
+
+    public static void RaiseStaticEvent()
+    {
+        PanelShowsEvent?.Invoke();
+
+    }
+
+    #endregion
+    
+    
+    
     public MainWindow()
     {
         InitializeComponent();
+        this.Position = new PixelPoint(0, MainWindowViewModel.TopMargin);
+        
+        PanelHidingTimer = new DispatcherTimer
+        {
+            Interval = TimeSpan.FromSeconds(1)
+        };
+        PanelHidingTimer.Tick += HidePanel;
+        //_lockedPosition = new Point(0, 300); // set the desired position
+        //PositionChanged += Window_PositionChanged;
+        DisplayHeight = Screens.Primary.Bounds.Height;
+        
     }
+
+    #region Button commands
+
+    public ICommand StartApplicationCommand { get; set; }
+
+    #endregion
+
+    /*private void WindowBase_OnPositionChanged(object? sender, PixelPointEventArgs e)
+    {
+        this.Position = new PixelPoint((int)_lockedPosition.X, (int)_lockedPosition.Y);
+        //PositionChanged="WindowBase_OnPositionChanged"
+    }*/
+
+    #region Methods
+
+    private void StartApp(string path)
+    {
+        try
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo
+            {
+                FileName = path,
+                UseShellExecute = true // Це важливо для запуску GUI додатків
+            };
+
+            Process process = new Process
+            {
+                StartInfo = startInfo
+            };
+
+            process.Start();
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Error while starting: {ex.Message}");
+        }
+    }
+
+    private void InputElement_OnPointerExited(object? sender, PointerEventArgs e)
+    {
+        PanelHidingTimer.Start();
+        // panel hiding
+    }
+
+    private void HidePanel(object sender, EventArgs e)
+    {
+        PanelHidingTimer.Stop();
+        //RaiseStaticEvent();
+        this.Position = new PixelPoint(-87, MainWindowViewModel.TopMargin);
+    }
+
+    private void InputElement_OnPointerEntered(object? sender, PointerEventArgs e)
+    {
+        this.Position = new PixelPoint(0, MainWindowViewModel.TopMargin);
+        RaiseStaticEvent();
+        var newButton = new Button
+        {
+            Height = 70,
+            Width = 70,
+            CornerRadius = new CornerRadius(15),
+            VerticalAlignment = VerticalAlignment.Center,
+            HorizontalAlignment = HorizontalAlignment.Center,
+            Margin = new Thickness(0,5,0,0)
+        };
+        WrapPanel.Children.Add(newButton);
+    }
+
+    #endregion
+    
 }
